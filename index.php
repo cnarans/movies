65faf959b47c987c4694b8febfb99cafcb693acb
Colin Narans <cnarans@gmail.com>
2016-07-05 09:56:35 -0500

    skeleton

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Movies!</title>
+	</head>
+	<body>
+		<div class = "wrapper">
+			<div class = "left">
+				<div class = "search">
+				</div>
+				<div class = "sidebar">
+				</div>
+			</div>
+			<div class = "center">
+			</div>
+			<div class = "right">
+				<div class = "profile">
+				</div>
+				<div class = "friends">
+				</div>
+				<div class = "chat">
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
