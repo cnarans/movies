289a9d75891f33a258efafe599b11af675608f9d
Colin Narans <cnarans@gmail.com>
2016-07-05 11:42:17 -0500

    friends

--- index.php
+++ index.php
@@ -18,11 +18,46 @@
 						Main
 					</div>
 					<div class = "navLink">
-						Playing
+						<img class = "navLink__button" src="play.png" alt="icon">
+						<div class = "navLink__text">Playing</div>
 					</div>
 					<div class = "navLink">
 						<img class = "navLink__button" src="play.png" alt="icon">
-						Discover
+						<div class = "navLink__text">Discover</div>
+					</div>
+					<div class = "navLink">
+						<img class = "navLink__button" src="play.png" alt="icon">
+						<div class = "navLink__text">Activity</div>
+					</div>
+					<div class = "navLink">
+						<img class = "navLink__button" src="play.png" alt="icon">
+						<div class = "navLink__text">Top Charts</div>
+					</div>
+					<div class = "navLink">
+						<img class = "navLink__button" src="play.png" alt="icon">
+						<div class = "navLink__text">New Releases</div>
+					</div>
+					<div class = "navTitle">
+						Your Movie Circles
+					</div>
+					<div class = "navLink">
+						<img class = "navLink__button" src="play.png" alt="icon">
+						<div class = "navLink__text">This I Must See</div>
+					</div>
+					<div class = "navLink">
+						<img class = "navLink__button" src="play.png" alt="icon">
+						<div class = "navLink__text">The Best</div>
+					</div>
+					<div class = "navTitle">
+						Collections
+					</div>
+					<div class = "navLink">
+						<img class = "navLink__button" src="play.png" alt="icon">
+						<div class = "navLink__text">Me and Bob</div>
+					</div>
+					<div class = "navLink">
+						<img class = "navLink__button" src="play.png" alt="icon">
+						<div class = "navLink__text">Travel Movies</div>
 					</div>
 				</div>
 			</div>
@@ -35,7 +70,8 @@
 						Release Date | Popularity
 					</div>
 					<div class = "buttons">
-						Buttons
+						<img class = "topbarButton" src="play.png" alt="icon">
+						<img class = "topbarButton" src="play.png" alt="icon">
 					</div>
 				</div>
 			</div>
@@ -44,6 +80,18 @@
 					Profile
 				</div>
 				<div class = "friends">
+					<div class = "friend">
+						<img class = "friend__pic" src = "face.svg" alt="profile">
+						<div>
+							<div class = "friend__name">
+								Robert Robertson
+							</div>
+							<div class = "friend_movie">
+								Jurassic Park
+							</div>
+						</div>
+						<img class = "friend__status" src="face.svg" alt="status">
+					</div>
 				</div>
 				<div class = "chat">
 					<div class = "searchLine">
