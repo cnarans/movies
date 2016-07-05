dcb5742fee772bff22101d844ffe05229ca48f1c
Colin Narans <cnarans@gmail.com>
2016-07-05 10:57:09 -0500

    populating

--- index.php
+++ index.php
@@ -3,23 +3,52 @@
 	<head>
 		<meta charset="utf-8"/>
 		<title>Movies!</title>
+		<link type="text/css" rel="stylesheet" href="style.css"/>
 	</head>
 	<body>
 		<div class = "wrapper">
 			<div class = "left">
 				<div class = "search">
+					<div class = "searchLine">
+						Search
+					</div>
 				</div>
 				<div class = "sidebar">
+					<div class = "navTitle">
+						Main
+					</div>
+					<div class = "navLink">
+						Playing
+					</div>
+					<div class = "navLink">
+						<img class = "navLink__button" src="play.png" alt="icon">
+						Discover
+					</div>
 				</div>
 			</div>
 			<div class = "center">
+				<div class = "topbar">
+					<div class = "discover">
+						Discover
+					</div>
+					<div class = "sort">
+						Release Date | Popularity
+					</div>
+					<div class = "buttons">
+						Buttons
+					</div>
+				</div>
 			</div>
 			<div class = "right">
 				<div class = "profile">
+					Profile
 				</div>
 				<div class = "friends">
 				</div>
 				<div class = "chat">
+					<div class = "searchLine">
+						Chat
+					</div>
 				</div>
 			</div>
 		</div>
